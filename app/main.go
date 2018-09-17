73b9a6e281ef99597cc5650090a13db0476f86ea
bartr <bartr@microsoft.com>
2018-09-17 13:58:14 -0500

    updated to work with latest version of logb

--- app/main.go
+++ app/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"io"
 	"log"
 	"net/http"
 	"os"
@@ -13,6 +12,7 @@ import (
 	"github.com/bartr/gohandlers/rawrequest"
 )
 
+// default values
 // this is the App Service shared CIFS folder
 var logPath = "/home/LogFiles/"
 var port = 8080
@@ -20,6 +20,11 @@ var port = 8080
 func main() {
 
 	parseCommandLine()
+
+	// this sets the log files for app and requests
+	if err := setupLogging(logPath); err != nil {
+		log.Fatal(err)
+	}
 
 	// setup handlers
 	http.HandleFunc("/requests", rawrequest.DisplayRawRequests)
@@ -44,7 +49,7 @@ func parseCommandLine() {
 
 	// parse flags
 	lfp := flag.String("logpath", "", "path to log files")
-	p := flag.Int("port", 8080, "port to listen on")
+	p := flag.Int("port", port, "port to listen on")
 	flag.Parse()
 
 	// set log path
@@ -57,60 +62,24 @@ func parseCommandLine() {
 		flag.Usage()
 		log.Fatal("invalid port")
 	}
+
 	port = *p
-
-	// setup log files
-	if err := setupAppLog(buildFullLogName(logPath, "app", ".log")); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := logb.SetLogFile(buildFullLogName(logPath, "request", ".log")); err != nil {
-		log.Fatal(err)
-	}
 }
 
-// setup log multi writer
-func setupAppLog(logFile string) error {
+// setupLogging - setup the app and request log files
+func setupLogging(logPath string) error {
 
-	// prepend date and time to log entries
-	log.SetFlags(log.Ldate | log.Ltime)
-
-	// open the log file
-	f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-
-	if err != nil {
+	// setup app log
+	if err := logb.SetupAppLog(logPath, "app", ".log"); err != nil {
 		return err
 	}
 
-	// setup a multiwriter to log to file and stdout
-	wrt := io.MultiWriter(os.Stdout, f)
-	log.SetOutput(wrt)
+	// setup request log
+	if err := logb.SetLogFile(logPath, "request", ".log"); err != nil {
+		return err
+	}
 
 	return nil
-}
-
-// build the full log file name
-// app services sets the WEBSITE_ROLE_INSTANCE_ID environment variable
-//   since we're writing to the CIFS share, we need to differentiate log file names
-//   in case there are multiple instances running
-func buildFullLogName(logPath string, logPrefix string, logExtension string) string {
-	if !strings.HasSuffix(logPath, "/") {
-		logPath += "/"
-	}
-
-	fileName := logPath + logPrefix
-
-	// use instance ID to differentiate log files between instances in App Services
-	if iid := os.Getenv("WEBSITE_ROLE_INSTANCE_ID"); iid != "" {
-		fileName += "_" + strings.TrimSpace(iid)
-	}
-
-	// make sure logExtension has a period
-	if !strings.HasPrefix(logExtension, ".") {
-		logExtension = "." + logExtension
-	}
-
-	return fileName + logExtension
 }
 
 // handle all requests
